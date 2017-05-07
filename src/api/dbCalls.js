b90ff94e2043b008fd897588171b79885545c33e
boseaditya21 <aditya@cloudkaptan.com>
2017-05-07 14:39:59 +0530

    User Login and Register

--- src/api/dbCalls.js
+++ src/api/dbCalls.js
@@ -25,6 +25,51 @@ var restClient=
 		});
 	});
   	return promise;
+	},
+
+	loginUser(reqForm)
+	{
+		console.log(reqForm);
+		var promise=new Promise(function(resolve,reject){
+			//SELECT password FROM account WHERE email='ab@gmail.com'
+			var q='SELECT password FROM account WHERE email='+"'"+reqForm.email+"'";
+			console.log(q);
+			connection.query(q,function(err,rows,fields){
+				if(!err)
+				{
+					console.log('The solution is: ', rows);
+					console.log(fields);
+					resolve(rows);
+				}	
+  				else
+  				{
+    				console.log('Error while performing Query.');
+    				reject(err);
+    				return;
+  				}
+			});
+		});
+		return promise;
+	},
+
+	newUser(reqForm)
+	{
+		console.log(reqForm);
+		var promise=new Promise(function(resolve,reject){
+			//INSERT INTO account(firstName, lastName, email, phone, address, password, securityQuestion, securityAnswer) VALUES ('Ananda','Bose','ab25@gmail.com','9748325149','6C, K.N.D. Lane, Kolkata - 700030','ab25','What is the capital of India?','Delhi')
+			var query = connection.query('INSERT INTO account SET ?', reqForm, function (error, results, fields) {
+  			if (error)
+  			{
+  				console.log(error);
+  				reject(error);
+  			}
+  			console.log(results);
+  			console.log(fields);
+  			resolve('Registered Successfully');
+		});
+		console.log(query.sql); // INSERT INTO posts SET `id` = 1, `title` = 'Hello MySQL' 
+		});
 	}
+
 }
 export default restClient;
--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import { version } from '../../package.json';
 import { Router } from 'express';
 import facets from './facets';
 import dbCalls from './dbCalls';
-var jwt    = require('jsonwebtoken');
+var jwt = require('jsonwebtoken');
 
 export default ({ config, db }) => {
 	let api = Router();
@@ -24,6 +24,53 @@ export default ({ config, db }) => {
 			res.end(str);
 		});
 		
+	});
+
+	api.post('/loginUser',(req,res)=>{
+		console.log('Before logging in');
+		console.log(req.body);
+		var promise = dbCalls.loginUser(req.body).then(function (str) {
+			str=JSON.stringify(str);
+			//console.log(str);
+			str=JSON.parse(str);
+			//console.log(str);
+			var keys=Object.keys(str);
+			for (var i = 0; i < keys.length; i++)
+			{
+  				var k=(str[keys[i]]);
+			}
+			console.log(k);
+			//res.end(JSON.stringify(k));
+			if(k.password!=req.body.password)
+			{
+				res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+			}
+			else
+			{
+				var token = jwt.sign(req.body, config.secret, {
+					expiresIn: "30d"
+				});
+				res.json({
+					success: true,
+					message: 'Enjoy your token!',
+					token: token,
+					k : k
+				});
+			}
+		},
+		function (str) {
+			res.end(str);
+		});
+	});
+
+	api.post('/newUser',(req,res)=>{
+		console.log('Before registering');
+		console.log(req.body);
+		var promise = dbCalls.newUser(req.body).then(function (str) {
+		},
+		function (str) {
+			res.end(str);
+		});
 	});
 
 	return api;
