9f0fa14a9b9e7a9d3ab22208fcd4a5fec4767cb2
boseaditya21 <aditya@cloudkaptan.com>
2017-05-23 10:00:02 +0530

    Personal Details Fetched and Apllied Loan Details fetched

--- src/api/dbCalls.js
+++ src/api/dbCalls.js
@@ -117,5 +117,98 @@ var restClient=
 		});
 		return promise;
 	},
+
+	updateImage(request)
+	{
+		console.log(reqForm);
+		var promise=new Promise(function(resolve,reject){
+			var query = connection.query('INSERT INTO image SET ?', reqForm, function (error, results, fields) {
+  			if(error)
+  			{
+  				console.log(error);
+  				reject(error);
+  			}
+  			console.log(results);
+  			console.log(fields);
+  			resolve('Registered Successfully');
+		});
+		});
+		return promise;
+	},
+
+	fetchPersonalDetails(request)
+	{
+		console.log(request);
+		var promise= new Promise(function(resolve,reject){
+			var q='SELECT firstName,lastName,email,phone,address,password,confirmPassword,securityQuestion,securityAnswer,gender,dob FROM account WHERE email='+request;
+			console.log(q);
+			connection.query(q,function(err,rows,fields){
+				if(!err)
+				{
+					console.log('The solution is: ', rows);
+					console.log(fields);
+					resolve(rows);
+				}	
+  				else
+  				{
+    				console.log('Error while performing Query.');
+    				reject(err);
+    				return;
+  				}
+			});
+		});
+		return promise;
+	},
+
+	updatePersonalDetails(request)
+	{
+		console.log(request);
+		var promise=new Promise(function(resolve,reject){
+			var sql="UPDATE application SET firstName=?,lastName=?,email=?,phone=?,address=?,gender=?,dob=? WHERE email=?"
+			console.log(sql);
+			console.log(request.phone);
+			connection.query(sql,[request.firstName,request.lastName,request.email,request.phone,request.address,request.gender,request.dob,request.email],function (err, result) {
+    			if (err)
+    			{
+    				console.log(err);
+    				throw err;
+    				reject("Unsuccessfull");
+    			}
+    			else
+    			{
+    				console.log(result.affectedRows + " record(s) updated");
+    				resolve("Updated Successfully");
+    			}
+  			});
+  		});
+		return promise;
+	},
+
+	dashboard(request)
+	{
+		console.log(request);
+		var promise= new Promise(function(resolve,reject){
+			var q='SELECT * FROM application WHERE email='+"'"+request+"'";
+			console.log(q);
+			connection.query(q,function(err,rows,fields){
+				if(!err)
+				{
+					console.log('The solution is: ', rows);
+					console.log(fields);
+					if(rows.length!=0)
+						resolve(rows);
+					else
+						resolve('Empty');
+				}	
+  				else
+  				{
+    				console.log('Error while performing Query.');
+    				reject(err);
+    				return;
+  				}
+			});
+		});
+		return promise;
+	},
 }
 export default restClient;
--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,11 @@ import facets from './facets';
 import dbCalls from './dbCalls';
 var jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
+/*var accountSid = 'AC152bc2caaed7aafc00903aed4b59fa03'; // Your Account SID from www.twilio.com/console
+var authToken = 'd17e7bffbf9bea048a599600f599e6ee';
+var twilio = require('twilio');
+var client = new twilio(accountSid, authToken);*/
+
 
 export default ({ config, db }) => {
 	let api = Router();
@@ -68,6 +73,41 @@ export default ({ config, db }) => {
 		console.log('Before registering');
 		console.log(req.body);
 		var promise = dbCalls.newUser(req.body).then(function (str) {
+			/*client.messages.create({
+    		body: 'Welcome to Online Loan Appliction',
+    		to: '+91'+req.body.phone,  // Text this number
+    		from: '+918240827330' // From a valid Twilio number
+			})
+			.then((message) => console.log(message.sid));*/
+			/*client.messages.create({ 
+    		to: "+91"+req.body.phone, 
+    		from: "+18177567987", 
+    		body: "This is the ship that made the Kessel Run in fourteen parsecs?", 
+}, function(err, message) { 
+	//console.log(message);
+    //console.log(message.sid);
+    console.log(err); 
+});*/
+			let transporter = nodemailer.createTransport({
+    			service: 'gmail',
+    			auth: {
+        			user: 'boseaditya1994@gmail.com',
+        			pass: 'aditya21'
+    			}
+			});
+				let mailOptions = {
+    				from: '"Aditya Bose" <boseaditya1994@gmail.com> ', // sender address
+    				to: req.body.email, // list of receivers
+    				subject: 'Welcome Aboard', // Subject line
+    				text: 'Hello! ?', // plain text body
+    				html: 'Hello <b>'+ req.body.firstName+',</b> <br> Welcome to Online Loan Application. <br> Thanks and Regards, <br> Aditya Bose' // html body
+			};
+			transporter.sendMail(mailOptions, (error, info) => {
+    		if (error) {
+        		return console.log(error);
+    		}
+    		console.log('Message %s sent: %s', info.messageId, info.response);
+			});
 			res.end(str);
 		},
 		function (str) {
@@ -146,6 +186,68 @@ export default ({ config, db }) => {
 			res.end(str);
 		});
 	});
+
+	api.post('/fetchPersonalDetails',(req,res)=>{
+		console.log('Before fetching');
+		console.log(req.body.parameter);
+		var promise = dbCalls.fetchPersonalDetails(req.body.parameter).then(function (str) {
+			str=JSON.stringify(str);
+			//console.log(str);
+			str=JSON.parse(str);
+			//console.log(str);
+			var keys=Object.keys(str);
+			for (var i = 0; i < keys.length; i++)
+			{
+  				var k=(str[keys[i]]);
+			}
+			console.log(k);
+			res.json({
+				details:k
+			})
+		},
+		function (str) {
+			res.end(str);
+		});
+	});
+
+	api.post('/updatePersonalDetails',(req,res)=>{
+		console.log('Before updating');
+		console.log(req.body);
+		var promise = dbCalls.updatePersonalDetails(req.body).then(function (str) {
+			res.end(str);
+		},
+		function (str) {
+			res.end(str);
+		});
+	});
+
+	api.post('/dashboard',(req,res)=>{
+		console.log('Before selecting');
+		console.log(req.body.parameter);
+		var promise = dbCalls.dashboard(req.body.parameter).then(function (str) {
+			res.end(str);
+		},
+		function (str) {
+			res.end(str);
+		});
+	});
+
+
+
+
+
+
+
+	/*api.post('/updateImage',(req,res)=>{
+		console.log('Before updating');
+		console.log(req.body);
+		var promise = dbCalls.updateImage(req.body).then(function (str) {
+			res.end(str);
+		},
+		function (str) {
+			res.end(str);
+		});
+	});*/
 
 	return api;
 }
